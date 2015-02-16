0fecef45ec2d6e4c09b84381e78a29862904925c
Dmytro <dbogatov@wpi.edu>
2015-02-15 22:29:26 -0500

    Change name.

--- Monopoly Banker/Tutorial/PageContentViewController.swift
+++ Monopoly Banker/Tutorial/PageContentViewController.swift
@@ -21,7 +21,7 @@ class PageContentViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.backgroundImageView.image = UIImage(named: self.imageFileName);
+        self.backgroundImageView.image = UIImage(named: "TutorialNumpad")//self.imageFileName);
         //self.backgroundImageView.layer.borderColor = UIColor.blueColor().CGColor
         //self.backgroundImageView.layer.borderWidth = 2.0
         
